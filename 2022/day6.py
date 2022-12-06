255b0b618ef5aafa510b833b9fb1e2c30a1361e9
vengadesh-m <96722271+vengadesh-m@users.noreply.github.com>
2022-12-06 07:20:11 +0000

    Day 6 Solution

---
+++ 2022/day6.py
@@ -0,0 +1,22 @@
+"Day 6 solution"
+import os
+from collections import deque
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SEP = os.path.sep
+
+_file = f"{BASE_DIR}{SEP}sample_inputs{SEP}Day6.txt"
+
+input_value = open(_file, encoding='utf-8').read()
+
+def find_first_signal(marker_count):    
+    "Return the Start of the marker"
+    queue = deque(maxlen=marker_count)
+    for num, value in enumerate(input_value):
+        queue.append(value)
+        if len(queue) == marker_count and len(set(queue)) == marker_count:
+            return num + 1
+
+
+print("Answer to Part 1 is: ", find_first_signal(4))
+print("Answer to Part 2 is: ", find_first_signal(14))
