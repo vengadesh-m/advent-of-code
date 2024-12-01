937608be12e72ea30506eed5aa083620355db6c0
vengadesh-m <venkatsmart92@gmail.com>
2024-12-01 11:06:50 +0530

    Day 1 rewrite

--- 2024/day01.py
+++ 2024/day01.py
@@ -8,7 +8,6 @@ SEP = os.path.sep
 file = f"{BASE_DIR}{SEP}sample_inputs{SEP}Day1.txt"
 
 with open(file, 'r', encoding='utf-8') as _file:
-    results = []
     first_list = []
     second_list = []
     for line in _file:
@@ -16,15 +15,11 @@ with open(file, 'r', encoding='utf-8') as _file:
         b = b.strip('\n')
         first_list.append(int(a))
         second_list.append(int(b))
-    # print(sorted(first_list), sorted(second_list))
     first_list = sorted(first_list)
     second_list = sorted(second_list)
-    for i in range(len(first_list)):
-        results.append(abs(second_list[i] - first_list[i]))
-    print(sum(results))
+    print(sum([abs(second_list[i] - first_list[i]) for i in range(len(first_list))]))
 
 with open(file, 'r', encoding='utf-8') as _file:
-    results = []
     first_list = []
     second_list = []
     for line in _file:
@@ -32,6 +27,4 @@ with open(file, 'r', encoding='utf-8') as _file:
         b = b.strip('\n')
         first_list.append(int(a))
         second_list.append(int(b))
-    for i in first_list:
-        results.append(i * second_list.count(i))
-    print(sum(results))
+    print(sum([i * second_list.count(i) for i in first_list]))
